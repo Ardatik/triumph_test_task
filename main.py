c5a5174f76a05bc76bf0408a02540b2cb2d06316
Ardatik <104901990+Ardatik@users.noreply.github.com>
2026-08-02 09:43:32 +0300

    Upload download server (#2)
    * add web interface
    
    * little fixes

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import logging
 
-from report.report_build import ReportBuilder
-
 logging.basicConfig(
     filename="app.log",
     level=logging.INFO,
-    format="%(asctime)s - %(levelname)s - %(message)s",
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     encoding="utf-8",
 )
 logger = logging.getLogger(__name__)
 
+
+import uvicorn
+
 if __name__ == "__main__":
-    builder = ReportBuilder()
-    builder.build_report(input_file="Тестовое_задание.xlsx")
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
